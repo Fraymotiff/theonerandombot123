0c40a3be0a38643783fc2b3b6853e33c4cad52b3
Fraymotiff <68887087+Fraymotiff@users.noreply.github.com>
2020-07-30 00:18:33 -0300

    first

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Discord = require ('discord.js');
 const client = new Discord.Client();
 
 
-client.once(' bot is now online!', () => {
+client.once('Russian Roulette bot is now online!', () => {
      //Code for the bot to do when starting up.
      })
 
@@ -14,22 +14,61 @@ const PREFIX ='!'
 client.once('ready', () => {
      //Code for the bot to do when starting up.
      })
-     console.log('random sender is now online!')
+     console.log('Cursed image sender is now online!')
    
      client.on('message', (message) => {
      const PREFIX = '!'
      
 
-     
-     if (message.content.startsWith (PREFIX + "random"))
-     let   = Math.floor(Math.random()* 179) 
-      message.channel.send ("will it be blessed? will it be cursed? Let's give it everything we got! It's random time!",  {files: ["./random/" + imageNumber + ".png"]} )
-   
+     if (message.content.startsWith (PREFIX + "cursed")) {
+         let imageNumber = Math.floor(Math.random()* 100) +1
+             message.channel.send ("Let's Give it everything we got! It's Punishment time!",  {files: ["./cursed/" + imageNumber + ".png"]} )
+             return; 
+          
               
              
              
      }
-    );
+     });
+    
+    
+
+
+
+    client.login(process.env.token);
+
+const Discord = require ('discord.js');
+
+const client = new Discord.Client();
+
+
+const PREFIX ='!'
+
+
+
+client.once('ready', () => {
+     //Code for the bot to do when starting up.
+     })
+     console.log('Cursed image sender is now online!')
+   
+     client.on('message', (message) => {
+     const PREFIX = '!'
+     
+
+   
+     if (message.content.startsWith (PREFIX + "cursed")) {
+          let imageNumber = Math.floor(Math.random()* 179) +1
+              message.channel.send ("Will it be blessed? will it be cursed? Let's give it everything we got! It's random time!",  {files: ["./random/" + imageNumber + ".png"]} )
+              return; 
+           
+               
+              
+          
+              
+             
+             
+     }
+     });
     
     
 
